c8e6651680c99f5f86761ec6291429b5d4d193ed
tusharx1805 <tusharx1810@gmail.com>
2025-07-18 19:51:41 +0530

    logout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -62,14 +62,16 @@ export const Header = () => {
               <div className="relative group">
                 <div className="p-2 bg-primary rounded-lg cursor-pointer">
                   <User className="h-5 w-5 text-primary-foreground" />
-                </div>
-                <div className="absolute right-0 mt-2 hidden group-hover:block bg-white shadow-lg rounded-md overflow-hidden z-10">
                   <button
                     onClick={handleLogout}
                     className="block w-full px-4 py-2 text-sm text-red-600 hover:bg-red-50 text-left"
                   >
                     Logout
                   </button>
+                </div>
+
+                <div className="absolute right-0 mt-2 hidden group-hover:block bg-white shadow-lg rounded-md overflow-hidden z-10">
+                  
                 </div>
               </div>
             </div>
